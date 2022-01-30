191f1b966783729b77914145b74142d6f530b0ff
julianne4 <91964634+julianne4@users.noreply.github.com>
2022-01-30 17:14:27 +0200

    Create animals.py

---
+++ brown_bag_code/animals.py
@@ -0,0 +1,27 @@
+#Create animal classes
+
+class Dog:
+    """A dog with a name and an age"""
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def description(self):
+        return f"{self.name} is {self.age} years old"
+
+    def speak(self, sound):
+        return f"{self.name} says woof!"
+  
+class Cat:
+    """A dog with a name and an age"""
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def description(self):
+        return f"{self.name} is {self.age} years old"
+
+    def speak(self, sound):
+        return f"{self.name} says meow!"
